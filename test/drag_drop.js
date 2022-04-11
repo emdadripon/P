86b2a8c0d0377b1dff933a5ef99f66c6df733504
emdadripon <eriponctg@gmail.com>
2022-04-11 00:33:01 -0400

    codeAdded
    Signed-off-by: emdadripon <eriponctg@gmail.com>

---
+++ test/drag_drop.js
@@ -0,0 +1,15 @@
+describe("mysuite", function(){
+
+    it("drag and drop", function(){
+        browser.get("https://codef0rmer.github.io/angular-dragdrop/#!/");
+        browser.manage().window().maximize();
+
+    var source_ele = element(by.model("list1"));
+    var target_ele = element(by.model("list2"));
+
+    browser.actions().dragAndDrop(source_ele, target_ele).perform();
+    browser.sleep(5000);
+
+    })
+
+})
